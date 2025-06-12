d0eb46f94d5b729e7062cf4b6667ab56abe01fe2
kishida-t <kishida-t@AXPC-1473N.TTRDOM.local>
2025-06-12 14:10:43 +0900

    1対1に変えました

--- src/main/java/jp/co/shiftw/dto/CategoriesDTO.java
+++ src/main/java/jp/co/shiftw/dto/CategoriesDTO.java
@@ -1,27 +1,24 @@
 package jp.co.shiftw.dto; // カテゴリーのDTO
 
 public class CategoriesDTO {
-	
+
 	private int categoryId;
 	private String name;
-	
-	
+
 	public int getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(int categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
-	
-	
 
 }
--- src/main/java/jp/co/shiftw/dto/ItemsDTO.java
+++ src/main/java/jp/co/shiftw/dto/ItemsDTO.java
@@ -1,72 +1,78 @@
-package jp.co.shiftw.dto;  //商品のDTO
+package jp.co.shiftw.dto; //商品のDTO
 
 public class ItemsDTO {
-	
+
 	private int itemId;
 	private String name;
 	private String manufacturer;
-	private int categoryId;
+	private CategoriesDTO categoryId;
 	private String color;
 	private int price;
 	private int stock;
 	private boolean recommended;
-	
-	
+
 	public int getItemId() {
 		return itemId;
 	}
+
 	public void setItemId(int itemId) {
 		this.itemId = itemId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getManufacturer() {
 		return manufacturer;
 	}
+
 	public void setManufacturer(String manufacturer) {
 		this.manufacturer = manufacturer;
 	}
-	public int getCategoryId() {
+
+	public CategoriesDTO getCategoryId() {
 		return categoryId;
 	}
-	public void setCategoryId(int categoryId) {
+
+	public void setCategoryId(CategoriesDTO categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getStock() {
 		return stock;
 	}
+
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
+
 	public boolean isRecommended() {
 		return recommended;
 	}
+
 	public void setRecommended(boolean recommended) {
 		this.recommended = recommended;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
