c4e38d5273ee65990ddb84cd4c23872d86be9ff6
uemura-n <uemura-n@AXPC-1471N.TTRDOM.local>
2025-06-12 13:47:48 +0900

    リファクタリング

--- src/main/java/jp/co/shiftw/dto/PurchasesDTO.java
+++ src/main/java/jp/co/shiftw/dto/PurchasesDTO.java
@@ -2,12 +2,13 @@ package jp.co.shiftw.dto;
 
 import java.sql.Date;
 
+// 購入履歴のDTO
 public class PurchasesDTO {
-	private int purchaseId;
-	private String purchasedUser;
-	private Date purchasedDate;
-	private String destination;
-	private boolean canse;
+	private int purchaseId; //注文ID
+	private String purchasedUser; //注文者
+	private Date purchasedDate; //注文日
+	private String destination; //配送先
+	private boolean canse; //キャンセル
 
 	public int getPurchaseId() {
 		return purchaseId;
