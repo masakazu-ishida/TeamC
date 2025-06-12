b839aaa4c9772dd46c312ef2d34ed4185bdf5dfa
shinya-t <shinya-t@AXPC-1474N.TTRDOM.local>
2025-06-12 13:35:46 +0900

    UsersDTOです!

---
+++ src/main/java/jp/co/shiftw/dto/UsersDTO.java
@@ -0,0 +1,36 @@
+package jp.co.shiftw.dto;
+
+public class UsersDTO {
+	
+	private String user_id;
+	private String password;
+	private String name;
+	private String address;
+
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	
+}
