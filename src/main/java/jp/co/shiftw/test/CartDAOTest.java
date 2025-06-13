8f91017285dffb9c725a0d5b2198742ce2e3d55e
oonishi-r <oonishi-r@AXPC-1472N.TTRDOM.local>
2025-06-13 15:09:45 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamC.git

--- src/main/java/jp/co/shiftw/test/CartDAOTest.java
+++ src/main/java/jp/co/shiftw/test/CartDAOTest.java
@@ -17,11 +17,18 @@ class CartDAOTest {
 	@Test
 	void test() {
 
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+
 		String sql = "SELECT items.name, items.color, items.manufacturer, items.price, items_in_cart.amount\n"
 				+ "From items_in_cart"
 				+ "INNER JOIN items"
 				+ "ON items_in_cart.item_id = items.item_id"
-				+ "WHERE user_id = ?";
+				+ "WHERE user_id = '?'";
 
 		List<CartDTO> cartList = new ArrayList<>();
 		try (Connection conn = DriverManager.getConnection(CommonConstants.LOOKUP_NAME)) {
