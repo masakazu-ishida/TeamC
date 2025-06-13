0e8008e369e69b3a449175b28bdb91bf6b99af86
oonishi-r <oonishi-r@AXPC-1472N.TTRDOM.local>
2025-06-13 14:20:54 +0900

    test

---
+++ src/main/java/jp/co/shiftw/dao/CartDAO.java
@@ -0,0 +1,62 @@
+package jp.co.shiftw.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.co.shiftw.dto.CartDTO;
+
+public class CartDAO extends BaseDAO {
+
+	public CartDAO(Connection conn) {
+		super(conn);
+	}
+
+	public List<CartDTO> CartList(String user_id) {
+
+		String sql = "SELECT items.name, items.color, items.manufacturer, items.price, items_in_cart.amount\n"
+				+ "From items_in_cart"
+				+ "INNER JOIN items"
+				+ "ON items_in_cart.item_id = items.item_id"
+				+ "WHERE user_id = ?";
+
+		List<CartDTO> cartList = new ArrayList<>();
+
+		try (PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			ps.setString(1, "user1");
+
+			System.out.println("接続完了");
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				//				CartDTO cart = new CartDTO();
+				//				ItemsDTO item = new ItemsDTO();
+
+				//				item.setName(rs.getString("name"));
+				//				item.setColor(rs.getString("color"));
+				//				item.setManufacturer(rs.getString("manufacturer"));
+				//				item.setPrice(rs.getInt("price"));
+				//				cart.setAmount(rs.getInt("amount"));
+				//				
+				//				cart.setItems(item);
+
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("color"));
+				System.out.println(rs.getString("manufacturer"));
+				System.out.println(rs.getInt("price"));
+				System.out.println(rs.getInt("amount"));
+
+				//				cart.setItems(item);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return cartList;
+	}
+
+}
---
+++ src/main/java/jp/co/shiftw/test/CartDAOTest.java
@@ -0,0 +1,20 @@
+package jp.co.shiftw.test;
+
+import org.junit.jupiter.api.Test;
+
+import jp.co.shiftw.dao.CartDAO;
+
+class CartDAOTest {
+
+	private C
+	
+	@Test
+	void test() {
+
+		System.out.println("test");
+		
+		CartDAO cartDao = new CartDAO(conn);
+
+	}
+
+}
