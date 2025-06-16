6c13a68472f4bcd7586e0967978624adadd5f3b5
wada-k <wada-k@AXPC-1556N.TTRDOM.local>
2025-06-16 15:17:58 +0900

    AdminSerivce

--- src/main/java/jp/co/shiftw/service/AdminService.java
+++ src/main/java/jp/co/shiftw/service/AdminService.java
@@ -1,9 +1,17 @@
 package jp.co.shiftw.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import jp.co.shiftw.dao.AdminDAO;
+import jp.co.shiftw.dto.AdminDTO;
 
 public class AdminService {
 
-	private AdminDAO admindao;
+	public static AdminDTO loginAdmin(Connection conn, String adminId, String password) throws SQLException {
+		AdminDAO dao = new AdminDAO(conn);
+
+		return dao.Login(adminId, password);
+	}
 
 }
