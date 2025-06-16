fa1d3131ef1863096cca99eeadd859bdffdec59e
kishida-t <kishida-t@AXPC-1473N.TTRDOM.local>
2025-06-16 15:03:32 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamC.git

--- src/main/java/jp/co/shiftw/dao/ItemsDAO.java
+++ src/main/java/jp/co/shiftw/dao/ItemsDAO.java
@@ -20,16 +20,14 @@ public class ItemsDAO extends BaseDAO {
 	//主キー検索を行うメソッド
 	public List<ItemsDTO> findByCond(int categoryId, String name) throws SQLException {
 
-		//キーワードとカテゴリを検索するSQL文
-		String sql = "select name, color, manufacturer, price from items where name = ? and category_id = ? or category_id * ? = 0";
+		//キーワードとカテゴリを検索するSQL文 「?」の中の数値が一緒であれば正しい範囲のフィールド値が出る
+		String sql = "select * from items where name = ? and category_id = ? or category_id * ? = 0";
 
 		ItemsDTO dto = null;
 
 		List<ItemsDTO> list = new ArrayList<>();
 
 		try (PreparedStatement ps = conn.prepareStatement(sql)) {
-
-			CategoriesDTO cate = new CategoriesDTO();
 
 			ps.setString(1, name);
 			ps.setInt(2, categoryId);
@@ -38,14 +36,22 @@ public class ItemsDAO extends BaseDAO {
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
 
+				CategoriesDTO cate = new CategoriesDTO();
+
+				cate.setCategoryId(rs.getInt("category_id"));
+				cate.setName(rs.getString("name"));
+
 				dto = new ItemsDTO();
 
 				dto.setName(rs.getString("name"));
 				dto.setCategory(cate);
+
+				list.add(dto);
 			}
 
 		}
 		return list;
 
 	}
+
 }
---
+++ src/main/java/jp/co/shiftw/test/ItemsDAOTest.java
@@ -0,0 +1,52 @@
+package jp.co.shiftw.test;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import jp.co.shiftw.dao.ItemsDAO;
+import jp.co.shiftw.dto.CategoriesDTO;
+import jp.co.shiftw.dto.ItemsDTO;
+import jp.co.shiftw.util.ConnectionUtil;
+
+class ItemsDAOTest {
+
+	@Test
+	void testFindByCond() {
+
+		try (Connection conn = ConnectionUtil.getConnectionForJUnit()) {
+
+			ItemsDAO dao = new ItemsDAO(conn);
+
+			try {
+
+				List<ItemsDTO> dtos = dao.findByCond(1, "麦わら帽子");
+
+				assertNotNull(dtos);
+
+				ItemsDTO dto = dtos.get(0);
+
+				CategoriesDTO category = dto.getCategory();
+
+				assertEquals(1, category.getCategoryId());
+				assertEquals("麦わら帽子", dto.getName());
+
+			} catch (Exception e) {
+
+				fail(e.getMessage());
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			fail(e.getMessage());
+
+		}
+
+	}
+
+}
