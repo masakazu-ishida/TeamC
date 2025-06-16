dcabce1e4619ff9dd633075d7a90de811ce2bf76
shinya-t <shinya-t@AXPC-1474N.TTRDOM.local>
2025-06-16 11:50:45 +0900

    わかんない

--- src/main/java/jp/co/shiftw/service/UsersService.java
+++ src/main/java/jp/co/shiftw/service/UsersService.java
@@ -1,5 +1,13 @@
 package jp.co.shiftw.service;
 
+import jp.co.shiftw.dao.UsersDAO;
+
 public class UsersService {
+
+	private UsersDAO usersDAO;
+
+	public UsersService(UsersDAO usersDAO) {
+		this.usersDAO = usersDAO;
+	}
 
 }
