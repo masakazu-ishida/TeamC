4d00201b2c5fe490c15c8c6c6aff81e7b850fe32
kishida-t <kishida-t@AXPC-1473N.TTRDOM.local>
2025-06-17 09:56:31 +0900

    Merge branch 'master' of https://github.com/masakazu-ishida/TeamC.git

--- src/main/java/jp/co/shiftw/service/ItemsSearchService.java
+++ src/main/java/jp/co/shiftw/service/ItemsSearchService.java
@@ -2,22 +2,44 @@ package jp.co.shiftw.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import jp.co.shiftw.dao.ItemsDAO;
 import jp.co.shiftw.dto.ItemsDTO;
+import jp.co.shiftw.util.CommonConstants;
+import jp.co.shiftw.util.ConnectionUtil;
 
 public class ItemsSearchService {
 
-	public static List<ItemsDTO> findByCond(Connection conn, int categoryId, String name) throws SQLException {
+	public static List<ItemsDTO> findByCond(int categoryId, String name) {
 
-		ItemsDAO dao = new ItemsDAO(conn);
+		List<ItemsDTO> list = new ArrayList<>();
 
-		return dao.findByCond(categoryId, name);
+		try (Connection conn = ConnectionUtil.getConnection(CommonConstants.LOOKUP_NAME)) {
 
-	}
+			conn.setAutoCommit(false);
 
-	{
+			try {
+				ItemsDAO dao = new ItemsDAO(conn);
+				list = dao.findByCond(categoryId, name);
+
+				conn.commit();
+
+			} catch (SQLException e) {
+
+				conn.rollback();
+				e.printStackTrace();
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
 
 	}
 
