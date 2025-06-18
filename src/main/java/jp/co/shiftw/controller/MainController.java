740ba788bf8e5b85925f56f950a8aa924a7b453f
oonishi-r <oonishi-r@AXPC-1472N.TTRDOM.local>
2025-06-18 13:40:09 +0900

    qq

--- src/main/java/jp/co/shiftw/controller/MainController.java
+++ src/main/java/jp/co/shiftw/controller/MainController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Main
  */
-@WebServlet("/Main")
+@WebServlet("/MainController")
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
