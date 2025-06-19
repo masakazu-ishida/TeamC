ac4c85fa6741d4d7df5217bd3e388f623d83f5be
wada-k <wada-k@AXPC-1556N.TTRDOM.local>
2025-06-19 10:10:44 +0900

    sf

--- src/main/java/jp/co/shiftw/service/CartAddService.java
+++ src/main/java/jp/co/shiftw/service/CartAddService.java
@@ -1,6 +1,10 @@
 package jp.co.shiftw.service;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
 
 import jp.co.shiftw.dao.CartDAO;
 import jp.co.shiftw.util.CommonConstants;
@@ -8,14 +12,21 @@ import jp.co.shiftw.util.ConnectionUtil;
 
 public class CartAddService {
 
-	public static void CartAdd() {
+	public static void CartAdd(String userId, int itemId, int amount, Date bookedDate) {
 
 		ConnectionUtil.mode = ConnectionUtil.MODE.TEST;
 		try (Connection conn = ConnectionUtil.getConnection(CommonConstants.LOOKUP_NAME)) {
 
 			CartDAO dao = new CartDAO(conn);
-		} catch (Exception e) {
-			// TODO: handle exception
+
+			dao.CartCerate(userId, itemId, amount, bookedDate);
+
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (ServletException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
 	}
 }
