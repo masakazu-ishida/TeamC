b17c367c2e115eadababa66cea962129fcd24849
wada-k <wada-k@AXPC-1556N.TTRDOM.local>
2025-06-19 09:13:05 +0900

    sgr

--- src/main/java/jp/co/shiftw/service/CartAddService.java
+++ src/main/java/jp/co/shiftw/service/CartAddService.java
@@ -1,10 +1,21 @@
 package jp.co.shiftw.service;
 
-import java.util.List;
+import java.sql.Connection;
 
-import jp.co.shiftw.dto.CartDTO;
+import jp.co.shiftw.dao.CartDAO;
+import jp.co.shiftw.util.CommonConstants;
+import jp.co.shiftw.util.ConnectionUtil;
 
 public class CartAddService {
 
-	public static List<CartDTO> Cart
+	public static void CartAdd() {
+
+		ConnectionUtil.mode = ConnectionUtil.MODE.TEST;
+		try (Connection conn = ConnectionUtil.getConnection(CommonConstants.LOOKUP_NAME)) {
+
+			CartDAO dao = new CartDAO(conn);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
