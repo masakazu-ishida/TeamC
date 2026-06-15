6bc16583a6fa06134f891c1cae41f79cc280dd7e
M-Tomo2394 <matsuura.t@ita-school.jp>
2026-06-15 09:48:43 +0900

    DTOの追加

---
+++ src/main/java/jp/co/sars/dto/PurchaseDetailsDTO.java
@@ -0,0 +1,42 @@
+package jp.co.sars.dto;
+
+public class PurchaseDetailsDTO {
+
+	//注文詳細テーブル
+	private int purchaseDetailId;//注文詳細ID(PK)
+	private int purchaseId;//注文ID(FK)
+	private int itemId;//商品ID
+	private int amount;//注文数
+
+	public int getPurchaseDetailId() {
+		return purchaseDetailId;
+	}
+
+	public void setPurchaseDetailId(int purchaseDetailId) {
+		this.purchaseDetailId = purchaseDetailId;
+	}
+
+	public int getPurchaseId() {
+		return purchaseId;
+	}
+
+	public void setPurchaseId(int purchaseId) {
+		this.purchaseId = purchaseId;
+	}
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
